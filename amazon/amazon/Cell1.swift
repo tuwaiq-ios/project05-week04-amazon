a7bcb2844d8b148cc3b889d1dfdd89f72153d7dc
saja Al-qhtani <saja-alqhtani@Mh-Mac.local>
2021-10-21 09:13:49 +0300

    by my mohammed

---
+++ amazon/amazon/Cell1.swift
@@ -0,0 +1,20 @@
+//
+//  Cell1.swift
+//  amazon
+//
+//  Created by mh on 11/03/1443 AH.
+//
+
+import UIKit
+
+class Cell1 : UICollectionViewCell {
+    
+    
+    @IBOutlet weak var logop: UIImageView!
+    
+    
+
+    @IBOutlet weak var lName: UILabel!
+    
+    @IBOutlet weak var price1: UILabel!
+}
---
+++ amazon/amazon/Cell2.swift
@@ -0,0 +1,15 @@
+//
+//  Cell2.swift
+//  amazon
+//
+//  Created by mh on 11/03/1443 AH.
+//
+import UIKit
+
+class Cell2 : UITableViewCell{
+    
+    @IBOutlet weak var labels: UILabel!
+    @IBOutlet weak var price2: UILabel!
+    @IBOutlet weak var Name2: UILabel!
+    @IBOutlet weak var Image1: UIImageView!
+}
